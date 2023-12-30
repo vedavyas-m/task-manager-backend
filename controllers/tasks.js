78b76423f60e98b7e70d71759222a77d00adb072
vedavyas-m <vedavyas.mhssv@gmail.com>
2023-12-31 02:00:42 +0530

    added task model, implemented getAllTasks and add task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,9 +1,18 @@
-const getAllTasks = (req, res) => {
-  res.send("<h1>get all tasks</h1>");
+const Task = require("../models/Task");
+
+const getAllTasks = async (req, res) => {
+  const tasks = await Task.find().select({
+    createdAt: 0,
+    updatedAt: 0,
+    __v: 0,
+  });
+  res.json(tasks);
 };
 
-const addTask = (req, res) => {
-  res.send("<h1>add task</h1>");
+const addTask = async (req, res) => {
+  //   console.log(req.body.task);
+  const task = await Task.create(req.body);
+  res.status(201).json(task);
 };
 
 const getTask = (req, res) => {
